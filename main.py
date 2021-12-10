07dcb258e6a790898c33590e4d1f6aa56433b6ec
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-12-10 20:19:20 +0530

    Remove copyright information

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Made with python3
-# (C) @FayasNoushad
-# Copyright permission under MIT License
-# All rights reserved by FayasNoushad
-# License -> https://github.com/FayasNoushad/Requote-URL-Bot/blob/main/LICENSE
-
 import os
 from requests.utils import requote_uri
 from pyrogram import Client, filters
